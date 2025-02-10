c1f485154b5e2154b80eab9e09b44161b8a18a93
Romeu Sorionaet <romeusoares14569@gmail.com>
2025-02-10 11:41:51 -0300

    config: first alteration - define global style and created page dashboard

---
+++ src/app/(our-city)/(dashboard)/page.tsx
@@ -0,0 +1,14 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <header>Header</header>
+
+      <main className="mt-44 text-center">
+        <h1>Dashboard</h1>
+        <p>Aprentação do projeto</p>
+
+        <button className="mt-44 border p-1">Ir para mapa</button>
+      </main>
+    </div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
 import ClientProviders from '@/utils/client-providers'
+import '@/assets/styles/globals.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
