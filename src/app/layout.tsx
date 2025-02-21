fafca1c6e9a5e35709eaca110a03472633c0e59a
Romeu Sorionaet <romeusoares14569@gmail.com>
2025-02-21 14:44:40 -0300

    refactor: added import path for tv4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import '@/assets/styles/base/globals.css'
 import ClientProviders from '@/utils/client-providers'
 
 const geistSans = Geist({
