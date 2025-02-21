9dcbea65b7bbfd99e078faba21d7834924776318
Romeu Sorionaet <romeusoares14569@gmail.com>
2025-02-21 14:38:14 -0300

    refactor: removed base folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import '@/assets/styles/base/globals.css'
+import '@/assets/styles/globals.css'
 import ClientProviders from '@/utils/client-providers'
 
 const geistSans = Geist({
