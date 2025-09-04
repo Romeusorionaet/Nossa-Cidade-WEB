424a18aaa2df664d73360f606214842ed3969860
Romeu Sorionaet <romeusoares14569@gmail.com>
2025-09-04 14:59:24 -0300

    refactor: removed func to fetch titles because, now It's fetch from style together

--- src/actions/services/maplibregl.ts
+++ src/actions/services/maplibregl.ts
@@ -1,7 +1,6 @@
 "use client";
 
 let cachedStyleUrl: string | null = null;
-let cachedTilesUrl: string | null = null;
 
 export async function maplibreglStyleCached(): Promise<string> {
   if (cachedStyleUrl) return cachedStyleUrl;
@@ -29,33 +28,5 @@ export async function maplibreglStyleCached(): Promise<string> {
     cachedStyleUrl = stadiaStyleUrl;
     localStorage.setItem(cacheKey, cachedStyleUrl);
     return cachedStyleUrl;
-  }
-}
-
-export async function maplibreglTilesCached(): Promise<string> {
-  if (cachedTilesUrl) return cachedTilesUrl;
-
-  const cacheKey = "mapTilesUrl";
-
-  const stored = localStorage.getItem(cacheKey);
-  if (stored) {
-    cachedTilesUrl = stored;
-    return cachedTilesUrl;
-  }
-
-  const maptilerTilesUrl = `https://api.maptiler.com/tiles/v3-openmaptiles/tiles.json?key=${process.env.NEXT_PUBLIC_MAPTILER_KEY}`;
-  const stadiaTilesUrl = "https://tiles.stadiamaps.com/data/openmaptiles.json";
-
-  try {
-    const response = await fetch(maptilerTilesUrl);
-    if (!response.ok) throw new Error("Falha no MapTiler");
-    cachedTilesUrl = maptilerTilesUrl;
-    localStorage.setItem(cacheKey, cachedTilesUrl);
-    return cachedTilesUrl;
-  } catch (e) {
-    console.warn("MapTiler falhou, usando StadiaMaps", e);
-    cachedTilesUrl = stadiaTilesUrl;
-    localStorage.setItem(cacheKey, cachedTilesUrl);
-    return cachedTilesUrl;
   }
 }
